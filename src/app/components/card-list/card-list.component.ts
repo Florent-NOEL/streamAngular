5c9cc515330b98b135ce04d0fa490172dae91a58
Florent-NOEL <121178866+Florent-NOEL@users.noreply.github.com>
2023-12-08 18:30:34 +0100

    add page controll home

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
 import { Router } from "@angular/router";
 import { urlSpringStream } from "src/app/env";
 import { VideoResponse } from "src/app/models/video-response";
@@ -9,25 +15,35 @@ import { VideoService } from "src/app/services/video.service";
   templateUrl: "./card-list.component.html",
   styleUrls: ["./card-list.component.css"],
 })
-export class CardListComponent implements OnInit {
+export class CardListComponent implements OnInit, OnChanges {
   url = urlSpringStream + "img/";
   videos!: Array<VideoResponse>;
   @Input()
   filteredVideo!: Array<VideoResponse>;
   @Input()
-  page = 0;
+  page!: number;
   @Input()
   items = 18;
   constructor(private videoServ: VideoService, private router: Router) {}
+
   ngOnInit(): void {
     if (this.filteredVideo == null) {
       this.findAVideoPage();
     }
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.findAVideoPage();
+  }
   findAVideoPage() {
     this.videoServ.findAllByPage(this.page, this.items).subscribe((data) => {
+      if (data.length == 0) {
+        this.page--;
+        console.log("page from card-list" + this.page);
+        return;
+      }
       this.videos = data;
       console.log(this.videos);
+      console.log("page from card-list" + this.page);
     });
   }
 
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+  page = 0;
+  doNext() {
+    this.page++;
+    console.log("page from home: " + this.page);
+  }
+  goBack() {
+    if (this.page > 0) {
+      this.page--;
+      console.log("page from home: " + this.page);
+    }
+  }
 }
